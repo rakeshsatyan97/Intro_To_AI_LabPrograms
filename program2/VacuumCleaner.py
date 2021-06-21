d3618efb3b9bc6f6125b4e9b78a6b6d5c73c38bd
rakeshsatyan97 <86243427+rakeshsatyan97@users.noreply.github.com>
2021-06-21 22:30:44 +0530

    Program 2

---
+++ program2/VacuumCleaner.py
@@ -0,0 +1,26 @@
+flag=True
+count=1
+while flag:
+    perc=input("enter the percept : ")
+    loc=input("enter the location : ")
+    if loc=="A":
+        if perc=="dirty":
+            print("action: suck...turn right")
+            
+        else:
+            print("action: turn right")
+            
+    else:
+        if perc=="dirty":
+            print("action: suck.....turn left")
+            
+        else:
+            print("action: turn left")
+    
+    print("Do you want to continue?")
+    Cont=input("Enter Y or N : ")
+    print()
+    if Cont == 'Y':
+         flag= True
+    else : 
+         flag = False
